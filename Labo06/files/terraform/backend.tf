db8c997e2dc2b438e093d4063f39f7df73cd67d1
dariovasq <dario.vasques@heig-vd.ch>
2024-05-31 15:33:01 +0200

    adds terraform files

--- Labo06/files/terraform/backend.tf
+++ Labo06/files/terraform/backend.tf
@@ -1,4 +1,8 @@
 terraform {
+  # Defining the global configuration for Terraform
   backend "local" {
+    # Using the local backend to store the state
+    # The state will be stored in a file named 'terraform.tfstate'
+    # in the directory where you run Terraform
   }
 }
--- Labo06/files/terraform/main.tf
+++ Labo06/files/terraform/main.tf
@@ -1,24 +1,27 @@
 provider "google" {
-  project     = var.gcp_project_id
-  region      = "europe-west6-a"
-  credentials = file("${var.gcp_service_account_key_file_path}")
+  project     = var.gcp_project_id # The GCP project ID
+  region      = "europe-west6-a" # The region used for resources
+  credentials = file("${var.gcp_service_account_key_file_path}") # Path to the GCP service account key file which will allow us to deploy resources to the cloud.
 }
 
 resource "google_compute_instance" "default" {
-  name         = var.gce_instance_name
-  machine_type = "f1-micro"
-  zone         = "europe-west6-a"
+  name         = var.gce_instance_name # The Google Compute Engine instance name
+  machine_type = "f1-micro" # The machine type for the instance
+  zone         = "europe-west6-a" # The zone in which the instance will be deployed
 
   metadata = {
-    ssh-keys = "${var.gce_instance_user}:${file("${var.gce_ssh_pub_key_file_path}")}"
+    # Set the ssh key for the instance
+    ssh-keys = "${var.gce_instance_user}:${file("${var.gce_ssh_pub_key_file_path}")}" 
   }
 
+  # Boot disk image for the instance
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
 
+  # Network configuration for the instance
   network_interface {
     network = "default"
 
@@ -29,21 +32,21 @@ resource "google_compute_instance" "default" {
 }
 
 resource "google_compute_firewall" "ssh" {
-  name          = "allow-ssh"
-  network       = "default"
-  source_ranges = ["0.0.0.0/0"]
+  name          = "allow-ssh" # Name of the firewall rule
+  network       = "default" # Network for the firewall rule
+  source_ranges = ["0.0.0.0/0"] # Source IP ranges allowed to access
   allow {
-    ports    = ["22"]
-    protocol = "tcp"
+    ports    = ["22"] # Allows SSH port
+    protocol = "tcp" # Protocol used by the rule
   }
 }
 
 resource "google_compute_firewall" "http" {
-  name          = "allow-http"
-  network       = "default"
-  source_ranges = ["0.0.0.0/0"]
+  name          = "allow-http" # Name of the firewall rule
+  network       = "default" # Network for the firewall rule
+  source_ranges = ["0.0.0.0/0"] # Source IP ranges allowed to access
   allow {
-    ports    = ["80"]
-    protocol = "tcp"
+    ports    = ["80"] # Allows HTTP port
+    protocol = "tcp" # Protocol used by the rule
   }
 }
